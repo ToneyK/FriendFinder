68af64a2755a3621410c315b9b525c5b4543536a
Your Name <you@example.com>
2018-04-24 18:07:40 -0600

    commit finished app

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Toney',
+    profilePic: 'https://www.50-best.com/mlb_team_wallpapers/pics/boston_red_sox.htm',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
